3aa85c8be75ce57a3b7bb143470c042d092c0899
Carlita <carla.severe@gmail.com>
2017-06-01 11:50:08 -0400

    using post method

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var config = require ('../config/config')
+var apiKey = 'fec8b5ab27b292a68294261bb21b04a5';
+
+
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	const apiKey = 'fec8b5ab27b292a68294261bb21b04a5';
-	const apiBaseUrl = 'http://api.themoviedb.org/3';
-	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey;
-	const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
-
 	request.get(nowPlayingUrl,(error, response, movieData)=>{
 		var movieData = JSON.parse(movieData);
 		//calling res.render in request callback 
@@ -19,9 +21,28 @@ router.get('/', function(req, res, next) {
 
 	});
 
-
-
  
+});
+
+// router.get('/search', (req, res)=>{
+// 	res.send("The get search page")
+// })
+
+router.post('/search', (req, res)=>{
+	// req.body is available bc of the body parser module
+	// bodyparser module installed when you created the express app
+	// req.body is where POSTED data will live. 
+	console.log(req.body);
+	// res.json(req.body);
+	//will hold whatever the user submitted 
+	var termUserSearchedFor = req.body.searchString
+	// res.send("The post search page")
+	var searchUrl = apiBaseUrl + '/search/movie?query='+termUserSearchedFor+'&api_key='+config.apiKey;
+	//passing variable along
+	request.get(searchUrl,(error, response, movieData)=>{
+		res.json(JSON.parse(movieData));
+
+	});
 });
 
 module.exports = router;
